ae9a383c2e3fba911bc70758c1ce6a2697355017
levnoppe <noppe.lev@gmail.com>
2021-04-16 19:58:46 +0300

    empty project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import * as data from '../data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'trivia';
+
+  ngOnInit(){
+    console.log(data);
+  }
 }
