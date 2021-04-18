28839e09694883df6a36c11472088cace2a39766
levnoppe <noppe.lev@gmail.com>
2021-04-18 07:39:00 +0300

    empty project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,43 @@ import * as data from '../data.json';
 })
 export class AppComponent implements OnInit{
   title = 'trivia';
+  number = 0;
+  questions: any = (data as any).default;
+  answered = false;
+  rightAnswer = false;
+  answeredButton = null;
 
   ngOnInit(){
     console.log(data);
+    this.answered = false;
+    this.answeredButton = null;
+  }
+
+  onAnswer(answer: number){
+    if (answer === this.questions[this.number].rightAnswer){
+      this.rightAnswer = true;
+    }
+    else {
+      this.rightAnswer = false;
+    }
+    this.answeredButton = answer;
+  }
+
+  getColor(answer: number): object {
+    if (this.answeredButton){
+      if (answer === this.answeredButton){
+        if (answer === this.questions[this.number].rightAnswer){
+          return {'background-color': 'green'};
+        } else {
+          return {'background-color': 'red'};
+        }
+      } else {
+        if (answer === this.questions[this.number].rightAnswer){
+          return {'background-color': 'darkSlateGray'};
+        } else {
+          return {'background-color': 'grey'};
+        }
+      }
+    }
   }
 }
